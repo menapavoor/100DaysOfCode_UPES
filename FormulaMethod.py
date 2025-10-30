003ba1df015b6ef2b62134a9b1ae7628ff7cd053
menapavoor <mena.pavoor@gmail.com>
2025-10-30 17:54:46 +0530

    Create FormulaMethod.py

---
+++ FormulaMethod.py
@@ -0,0 +1,10 @@
+# Program to find sum of first n natural numbers
+
+# Step 1: Input
+n = int(input("Enter a number: "))
+
+# Step 2: Formula
+total = n * (n + 1) // 2   # integer division
+
+# Step 3: Output
+print("Sum =", total)
